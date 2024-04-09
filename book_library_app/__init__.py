7f1f83d7a65d913300866e9462ed4b99fa774cda
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2024-04-09 20:36:40 +0200

    app with mysql connected

--- book_library_app/__init__.py
+++ book_library_app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
 from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+with app.app_context():
+    result = db.session.execute(text('show tables;'))
+
+for row in result:
+    print(row)
 
 
 from book_library_app import authors
