9e45868d89d0e2f1120b99f4b5cb917464d5a8b9
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2024-04-09 20:53:39 +0200

    author model created and first migrate

--- book_library_app/__init__.py
+++ book_library_app/__init__.py
@@ -1,20 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
+from flask_migrate import Migrate
 
 from config import Config
 
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-
-with app.app_context():
-    result = db.session.execute(text('show tables;'))
-
-for row in result:
-    print(row)
+migrate = Migrate(app, db)
 
 
 from book_library_app import authors
+from book_library_app import models
