8ad8b10bb575dd7459a09dee5231caba8a9f9d7b
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2024-04-11 20:26:01 +0200

    code refactoring

--- book_library_app/commands/db_manage_commands.py
+++ book_library_app/commands/db_manage_commands.py
@@ -4,8 +4,16 @@ from datetime import datetime
 from sqlalchemy.sql import text
 
 from book_library_app import db
-from book_library_app.models import Author
+from book_library_app.models import Author, Book
 from book_library_app.commands import db_manage_bp
+
+
+def load_json_data(file_name: str) -> list:
+    json_path = Path(__file__).parent.parent / 'samples' / file_name
+    with open(json_path) as file:
+        data_json = json.load(file)
+    
+    return data_json
 
 
 @db_manage_bp.cli.group()
@@ -16,30 +24,34 @@ def db_manage():
 
 @db_manage.command()
 def add_data():
-    '''Add sample data to database'''
+    """Add sample data to databases"""
     try:
-        authors_path = Path(__file__).parent.parent / 'samples' / 'authors.json'
-        with open(authors_path) as file:
-            data_json = json.load(file)
-
+        data_json = load_json_data('authors.json')
         for item in data_json:
             item['birth_date'] = datetime.strptime(item['birth_date'], '%d-%m-%Y').date()
             author = Author(**item)
             db.session.add(author)
+
+        data_json = load_json_data('books.json')
+        for item in data_json:
+            book = Book(**item)
+            db.session.add(book)
+
         db.session.commit()
-        print('Data has been successfully added to database')
-    
+        print('Data has been succesffully added to database')
     except Exception as exc:
-        print('unexpected error: {}'.format(exc))
+        print("unexpected error: {}".format(exc))
 
 
 @db_manage.command()
 def remove_data():
-    '''Remove all data from database'''
+    """Remove all data from databases"""
     try:
-        db.session.execute(text('TRUNCATE TABLE authors'))
+        db.session.execute(text('DELETE FROM books'))
+        db.session.execute(text('ALTER TABLE books AUTO_INCREMENT = 1'))
+        db.session.execute(text('DELETE FROM authors'))
+        db.session.execute(text('ALTER TABLE authors AUTO_INCREMENT = 1'))
         db.session.commit()
-        print('Data has been successfully removed from database')
-    
+        print('Data has been succesffully removed from database')
     except Exception as exc:
-        print('unexpected error: {}'.format(exc))
+        print("unexpected error: {}".format(exc))
